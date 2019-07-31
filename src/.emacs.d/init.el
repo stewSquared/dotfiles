215ac3cf337e57b56177542ec1f0ba1fb7b1279f
Stewart Stewart <stewinsalot@gmail.com>
2019-07-30 22:09:02 -0700

    Remove suprious config added by Custom

--- src/.emacs.d/init.el
+++ src/.emacs.d/init.el
@@ -126,17 +126,3 @@
 (setq inhibit-startup-screen t)
 (menu-bar-mode -1)
 (setq mouse-yank-at-point t)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (xclip smartparens rainbow-delimiters markdown-mode magit go-mode geiser ensime elm-mode ac-cider))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
