0c8c9fe757e5e8d3ead1df78985cb4e2af934e8c
Stewart Stewart <stewinsalot@gmail.com>
2019-07-31 23:27:28 -0700

    Remove ac-cider (deprecated in favor of company-mode)

--- src/.emacs.d/init.el
+++ src/.emacs.d/init.el
@@ -33,7 +33,6 @@
 (install 'cider)
 (install 'geiser)
 (install 'rainbow-delimiters)
-(install 'ac-cider)
 (install 'elm-mode)
 (install 'magit)
 (install 'projectile)
@@ -55,15 +54,6 @@
 
 ;; cider config
 (add-hook 'cider-repl-mode-hook #'rainbow-delimiters-mode)
-
-(require 'ac-cider)
-(add-hook 'cider-mode-hook 'ac-flyspell-workaround)
-(add-hook 'cider-mode-hook 'ac-cider-setup)
-(add-hook 'cider-repl-mode-hook 'ac-cider-setup)
-(eval-after-load "auto-complete"
-  '(progn
-     (add-to-list 'ac-modes 'cider-mode)
-     (add-to-list 'ac-modes 'cider-repl-mode)))
 
 ;; ensime config
 (require 'ensime)
